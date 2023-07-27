ba77b92058790886c380bb36c9562b2bff45dcaa
kalebe <kalebe613@gmail.com>
2023-07-27 14:12:40 +0100

    Create list tasks

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import TaskModel
+@admin.register(TaskModel)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['description', 'updated']
--- task/models.py
+++ task/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class TaskModel(models.Model):
+    created = models.DateTimeField('Created',auto_now_add=True)
+    updated = models.DateTimeField('Updated', auto_now=True)
+    description = models.CharField('Description',max_length=100)
+    completed = models.BooleanField('Completed', default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+    
+    class Meta:
+        verbose_name = ('description')
+        verbose_name_plural = ('descriptions')
+
