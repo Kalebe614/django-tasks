4ef538f05febf0fd1c15e21c3b3eebc0aaf0a3b3
kalebe <kalebe613@gmail.com>
2023-08-03 10:38:40 +0100

    Create Edit Todo

--- task/urls.py
+++ task/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
    path('', views.TaskView.as_view(), name='task'),
    path('create/', views.TaskCreateView.as_view(), name='create'),
    path('delete/<int:pk>/', views.TaskDeleteView.as_view(), name='delete'),
+   path('updateCompleted/<int:pk>/', views.TaskUpdateCompletedView.as_view(), name='updateCompleted'),
    path('update/<int:pk>/', views.TaskUpdateView.as_view(), name='update'),
 ]
