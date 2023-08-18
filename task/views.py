b859c5c4db725ab2b374ebd9f81709f4e9c38329
Kalebe de Oliveira Freitas <kalebe613@gmail.com>
2023-08-18 19:56:09 +0100

    Update views.py

--- task/views.py
+++ task/views.py
@@ -16,8 +16,8 @@ class TaskView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['selected_filter_option'] = self.request.GET.get('filter', 'default_filter_option')
-        context['selected_sort_option'] = self.request.GET.get('sort', 'default_sort_option')
+        context['selected_filter_option'] = self.request.GET.get('filter', 'all')
+        context['selected_sort_option'] = self.request.GET.get('sort', 'added')
         return context
 
     def get_queryset(self):
